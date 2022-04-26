d33137e49b1e7592016c420897bf1d4119d793ec
johnson obichukwu <obiejohn24@gmail.com>
2022-04-26 04:31:50 +0100

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,17 @@
 #include "main.h" 
+  
  /** 
   * _strlen - find the length of a string 
   * @s: pointer to the string to check 
   * Return: void 
- */ 
+  */ 
   
   
  int _strlen(char *s) 
  { 
          int i = 0; 
   
-         whil (s[i]) 
+         while (s[i]) 
                  i++; 
   
          return (i); 
