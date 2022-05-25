a1345e777c31aac95a087f649bcc114dbc19cddf
johnson obichukwu <obiejohn24@gmail.com>
2022-05-25 18:42:16 +0100

    Putchar file

--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -1,13 +1,14 @@
+#include "lists.h"
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: the character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: on sucess 1
+ * On error: -1 is returned and errno is set appropriately
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	rturn (write (1, &c, !));
 }
